f7f9b49ad94467c39da0e3cf8b85c7416587faf2
Khuna Chuan <chuan_8297@hotmail.com>
2018-10-31 19:11:04 +0700

    Create movie model and Update movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,21 @@
 class MoviesController < ApplicationController
 
   def index
-    # @movies = Movie.all
+    @movies = Movie.all
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.create!(allowed_params)
+    redirect_to movies_path
+  end
+
+  def allowed_params
+    params.require(:movie)
+    params[:movie].permit(:title,:rating,:release_date)
   end
 
 end
