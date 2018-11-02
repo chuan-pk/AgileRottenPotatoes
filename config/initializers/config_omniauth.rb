5127d082c1a1f6e87eaa273660777fc5340133b1
Khuna Chuan <chuan_8297@hotmail.com>
2018-11-02 23:49:04 +0700

    add github login

--- config/initializers/config_omniauth.rb
+++ config/initializers/config_omniauth.rb
@@ -1,4 +1,5 @@
 # Replace API_KEY and API_SECRET with the values you got from Twitter
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
+  provider :github, ENV['GITHUB_APP_ID'], ENV['GITHUB_APP_SECRET']
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :movies
-  root(:to => redirect('/welcome'))
-  get '/welcome' => 'movies#welcome'
+  root :to => "movies#welcome"
   get  'auth/:provider/callback' => 'sessions#create'
   post 'logout' => 'sessions#destroy'
   get  'auth/failure' => 'sessions#failure'
-  get  'auth/facebook', :as => 'login'
+  get  'auth/facebook', :as => 'login_facebook'
+  get  'auth/github', :as => 'login_github'
 end
