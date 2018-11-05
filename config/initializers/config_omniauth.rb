073986bcdcf6d0793ad0452fe7aafa22150c2208
Khuna Chuan <chuan_8297@hotmail.com>
2018-11-06 00:09:00 +0700

    update config/initializer/config_omninauth.rb

--- config/initializers/config_omniauth.rb
+++ config/initializers/config_omniauth.rb
@@ -1,5 +1,5 @@
 # Replace API_KEY and API_SECRET with the values you got from Twitter
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
-  provider :github, ENV['GITHUB_APP_ID'], ENV['GITHUB_APP_SECRET']
+  provider :facebook, 'FACEBOOK_APP_ID', 'FACEBOOK_APP_SECRET'
+  provider :github, 'GITHUB_APP_ID', 'GITHUB_APP_SECRET'
 end
